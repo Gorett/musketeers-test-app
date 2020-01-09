732cf17423c6d769417306b9030ff2ffe8864b81
Oleksii Chuian <aleksey.chuyan@gmail.com>
2020-01-09 17:01:53 +0200

    [master] bookListMock added

--- components/BookCardList.js
+++ components/BookCardList.js
@@ -2,6 +2,22 @@ import React from 'react';
 
 import styles from '../styles/components/BookCardList.less';
 
+// TODO load data from service
+const bookListMock = [{
+    title: 'In Search of Lost Time',
+    author: 'Marcel Proust',
+    lentAt: '',
+    returnedAt: null,
+    borrower: 'Will Smith',
+},
+    {
+        title: 'One Hundred Years of Solitude',
+        author: 'Gabriel Garcia Marquez',
+        lentAt: '',
+        returnedAt: '',
+        borrower: 'Matt Damon',
+    }];
+
 export const BookCardList = () => (<div className={styles['book-card-list']}>Book Card List</div>);
 
 export default BookCardList;
