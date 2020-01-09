0fdaf73f227a4eb730a01d609f5cd18be0e256af
Oleksii Chuian <aleksey.chuyan@gmail.com>
2020-01-09 17:10:24 +0200

    [master] BookCard component used for render list of the book cards

---
+++ components/BookCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const BookCard = () => {
+    return (<p>BookCard component</p>)
+};
+
+export default BookCard;
--- components/BookCardList.js
+++ components/BookCardList.js
@@ -1,23 +1,34 @@
 import React from 'react';
 
+import BookCard from "./BookCard";
+
 import styles from '../styles/components/BookCardList.less';
 
 // TODO load data from service
-const bookListMock = [{
-    title: 'In Search of Lost Time',
-    author: 'Marcel Proust',
-    lentAt: '',
-    returnedAt: null,
-    borrower: 'Will Smith',
-},
+const bookListMock = [
     {
+        id: '123hfas',
+        title: 'In Search of Lost Time',
+        author: 'Marcel Proust',
+        lentAt: null,
+        returnedAt: null,
+        borrower: 'Will Smith',
+    },
+    {
+        id: '4h12k1s',
         title: 'One Hundred Years of Solitude',
         author: 'Gabriel Garcia Marquez',
-        lentAt: '',
-        returnedAt: '',
+        lentAt: null,
+        returnedAt: null,
         borrower: 'Matt Damon',
     }];
 
-export const BookCardList = () => (<div className={styles['book-card-list']}>Book Card List</div>);
+export const BookCardList = () => (
+    <div className={styles['book-card-list']}>
+        {bookListMock.map(book => (
+            <BookCard key={`${book.id}-key`} {...book} />
+        ))}
+    </div>
+);
 
 export default BookCardList;
