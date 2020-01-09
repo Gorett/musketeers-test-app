754572ba22c8e1056ec4c2d0267028872fb176bc
Oleksii Chuian <aleksey.chuyan@gmail.com>
2020-01-09 15:39:00 +0200

    [master] Index page updated. BookCardList component added

---
+++ components/BookCardList.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const BookCardList = () => (<div>Book Card List</div>);
+
+export default BookCardList;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import BookCardList from '../components/BookCardList';
+
 
 const IndexPage = () => {
     return (
         <div>
-            <h1>Index page</h1>
+            <header>
+                <h1>Books that I have lent to friends</h1>
+            </header>
+            <BookCardList />
+            <footer>
+                <button>Add new book</button>
+            </footer>
         </div>
     )
 };
